0cf5cf3e780bb6114cb284b8212bb3f7ef28688e
dmytrobaida <dmytrobaida@outlook.com>
2024-02-11 22:45:52 +0200

    fixes paths to support gh-pages

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+    base: './',
     root: __dirname,
     cacheDir: '../../node_modules/.vite/apps/website',
 
